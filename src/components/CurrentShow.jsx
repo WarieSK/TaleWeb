ec99383168c800f37bee177adc0c3fca36f026be
WarieSK <polansky.gabriel@gmail.com>
2025-03-12 19:49:23 +0100

    currentshow optimalization

--- src/components/CurrentShow.jsx
+++ src/components/CurrentShow.jsx
@@ -47,17 +47,19 @@ const CurrentShow = () => {
                 <th>Opis:</th>
                 <td>
                   <div 
-                    className={`current-show-description ${isExpanded ? 'expanded' : ''}`} 
-                    style={{ 
-                      maxHeight: isMobile ? (isExpanded ? 'none' : '100px') : 'none', 
-                      overflow: isMobile ? 'hidden' : 'visible', 
-                      transition: 'max-height 0.3s ease-in-out' 
-                    }}
+                    className={`current-show-description ${isExpanded ? 'expanded' : 'collapsed'}`} 
                   >
                     <p>
-                    Profesor Physicus vynájde mechanického človeka, ktorému dá meno Železník. Železník je silný, vytrvalý a šikovný a pán veľkopodnikateľ, ktorý si robota objednal, ho chce využiť na prácu v továrni, aby mohol prepustiť ostatných zamestnancov. Železníkovi sa však táto úloha nepáči, pretože by sa chcel stať človekom, na to však potrebuje srdce. Počas plesu, ktorý usporiada veľkopodnikateľ na oslavu zásnub svojej dcéry Flóry, sa nechá oklamať zlodejskou bandou, ktorá mu prisľúbi zlaté srdce a začne s nimi lúpiť. Jeho púť za srdcom sa však u zlodejov nekončí. Po tom, ako oslobodí unesenú Flóru, dostane sa do galérie, kde stretá Monu Lisu, Nefertiti, Fridu Kalo, Venušu a Apolóna i ďalšie umelecké diela. Ani tam však srdce nenájde. Až láska mu dá to, čo hľadá.
+                      Profesor Physicus vynájde mechanického človeka, ktorému dá meno Železník. 
+                      Železník je silný, vytrvalý a šikovný a pán veľkopodnikateľ, ktorý si robota 
+                      objednal, ho chce využiť na prácu v továrni, aby mohol prepustiť ostatných zamestnancov. 
+                      Železníkovi sa však táto úloha nepáči, pretože by sa chcel stať človekom, na to však potrebuje srdce. 
+                      Počas plesu, ktorý usporiada veľkopodnikateľ na oslavu zásnub svojej dcéry Flóry, sa nechá oklamať 
+                      zlodejskou bandou, ktorá mu prisľúbi zlaté srdce a začne s nimi lúpiť. 
+                      Jeho púť za srdcom sa však u zlodejov nekončí. Po tom, ako oslobodí unesenú Flóru, 
+                      dostane sa do galérie, kde stretá Monu Lisu, Nefertiti, Fridu Kalo, Venušu a Apolóna i ďalšie umelecké diela. 
+                      Ani tam však srdce nenájde. Až láska mu dá to, čo hľadá.
                     </p>
-                    
                   </div>
                   {isMobile && (
                     <button className="show-more-button" onClick={toggleDescription}>
